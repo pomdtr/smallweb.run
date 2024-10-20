5552e0bd857d1a931c78f7822a4c223b9d7b473e
pomdtr <achille.lacoin@gmail.com>
2024-10-20 20:57:44 +0000

    update 2024-10-20

--- cloud/main.ts
+++ cloud/main.ts
@@ -42,9 +42,9 @@ const waitlist = /* html */ `
         <p class="mb-4">Be among the first to try it out!</p>
         <form id="waitlist-form" class="space-y-5">
             <div>
-                <input type="email" id="email" placeholder="Email" name="email" required class="input mt-1 block w-full px-3 py-2 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm bg-gray-700 text-gray-300 border border-gray-600">
+                <input type="email" id="email" placeholder="Email" name="email" required class="input mt-1 block w-full px-3 py-2 rounded-md shadow-sm focus:outline-none focus:ring-pink-500 focus:border-pink-500 sm:text-sm bg-gray-700 text-gray-300 border border-gray-600">
             </div>
-            <button type="submit" class="button w-full py-2 px-4 rounded-md bg-gray-700 text-gray-300 hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Join</button>
+            <button type="submit" class="button w-full py-2 px-4 rounded-md bg-pink-500 text-gray-900 hover:bg-pink-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500">Join</button>
         </form>
     </div>
     <script>
