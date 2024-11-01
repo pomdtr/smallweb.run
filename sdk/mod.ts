dfdb0875407feac0230150e0fd33a9a7acb04029
pomdtr <achille.lacoin@gmail.com>
2024-11-01 12:07:19 +0000

    1 Nov 2024, 13:07

--- sdk/mod.ts
+++ sdk/mod.ts
@@ -90,6 +90,12 @@ const swaggerHomepage = /* html */ `<!DOCTYPE html>
     <link rel="stylesheet" type="text/css" href="https://raw.esm.sh/swagger-ui-dist@5.17.14/index.css" />
     <link rel="icon" type="image/png" href="https://raw.esm.sh/swagger-ui-dist@5.17.14/favicon-32x32.png" sizes="32x32" />
     <link rel="icon" type="image/png" href="https://raw.esm.sh/swagger-ui-dist@5.17.14/favicon-16x16.png" sizes="16x16" />
+    <style>
+      /* Hide the top black bar */
+      .swagger-ui .topbar {
+        display: none;
+      }
+  </style>
   </head>
 
   <body>
