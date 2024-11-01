7dee042d8be970de848e60b5d8c8ce0b5f5c1b09
pomdtr <achille.lacoin@gmail.com>
2024-11-01 12:01:10 +0000

    1 Nov 2024, 13:01

--- sdk/mod.ts
+++ sdk/mod.ts
@@ -80,6 +80,25 @@ type App = {
 }
 
 const swaggerHomepage = /* html */ `<!DOCTYPE html>
+<!-- HTML for static distribution bundle build -->
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>Swagger UI</title>
+    <link rel="stylesheet" type="text/css" href="https://raw.esm.sh/swagger-ui-dist@5.17.14/swagger-ui.css" />
+    <link rel="stylesheet" type="text/css" href="https://raw.esm.sh/swagger-ui-dist@5.17.14/index.css" />
+    <link rel="icon" type="image/png" href="https://raw.esm.sh/swagger-ui-dist@5.17.14/favicon-32x32.png" sizes="32x32" />
+    <link rel="icon" type="image/png" href="https://raw.esm.sh/swagger-ui-dist@5.17.14/favicon-16x16.png" sizes="16x16" />
+  </head>
+
+  <body>
+    <div id="swagger-ui"></div>
+    <script src="https://raw.esm.sh/swagger-ui-dist@5.17.14/swagger-ui-bundle.js" charset="UTF-8"> </script>
+    <script src="https://raw.esm.sh/swagger-ui-dist@5.17.14/swagger-ui-standalone-preset.js" charset="UTF-8"> </script>
+    <script src="https://raw.esm.sh/swagger-ui-dist@5.17.14/swagger-initializer.js" charset="UTF-8"> </script>
+  </body>
+</html>
 `
 
 export function api(): App {
