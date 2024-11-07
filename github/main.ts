a1369d8ef899ebec1125aad48cda6bf56cc8dc89
pomdtr <achille.lacoin@gmail.com>
2024-11-07 19:03:30 +0100

    7 Nov 2024, 19:03

--- github/main.ts
+++ github/main.ts
@@ -1,3 +1,5 @@
+import { open } from "jsr:@smallweb/open@0.1.1"
+
 export default {
     fetch: (req: Request) => {
         const url = new URL(req.url)
@@ -6,5 +8,18 @@ export default {
         }
 
         return Response.redirect("https://github.com/pomdtr/smallweb.run")
+    },
+    run: async (args: string[]) => {
+        if (args.length == 0) {
+            await open("https://github.com/pomdtr/smallweb.run")
+            return
+        }
+
+        let filepath = args[0]
+        if (!filepath.startsWith("/")) {
+            filepath = `/${filepath}`
+        }
+
+        await open(`https://github.com/pomdtr/smallweb.run/tree/main${filepath}`)
     }
 }
