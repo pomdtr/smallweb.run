217530bcc2dc73e5d3cdb8d8a246958647a77d90
pomdtr <achille.lacoin@gmail.com>
2024-11-12 16:37:18 +0100

    12 Nov 2024, 16:37

--- email/main.ts
+++ email/main.ts
@@ -1,10 +1,22 @@
-import { ValTown } from "@valtown/sdk";
+import { ValTown } from "npm:@valtown/sdk@0.23.0";
+import { Command } from "jsr:@cliffy/command@1.0.0-rc.7";
+
+const { SMALLWEB_APP_NAME } = Deno.env.toObject();
 
 export default {
-    run: (args: string[]) => {
-        const valtown = new ValTown();
-        valtown.emails.send({
-            text: args.join(" "),
-        });
+    async run(args: string[]) {
+        const command = new Command().name(SMALLWEB_APP_NAME).version("0.1.0")
+            .option(
+                "-m, --message <message:string>",
+                "The message to send",
+                { required: true },
+            ).action(async (options) => {
+                const vt = new ValTown();
+                await vt.emails.send({
+                    text: options.message,
+                });
+            });
+
+        await command.parse(args);
     },
 };
