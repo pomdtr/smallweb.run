2b5c3b797455197ae379178ed34a151e0b8a3ccc
pomdtr <achille.lacoin@gmail.com>
2024-11-13 23:41:14 +0100

    13 Nov 2024, 23:41

--- raw/main.ts
+++ raw/main.ts
@@ -1,6 +1,6 @@
 export default {
     fetch: (req: Request) => {
         const url = new URL(req.url);
-        return Response.redirect(`https://raw.githubusercontent.com/pomdtr/smallweb.run/refs/heads/main${url.pathname}`);
+        return fetch(`https://raw.githubusercontent.com/pomdtr/smallweb.run/refs/heads/main${url.pathname}`)
     }
 }
--- vscode/main.ts
+++ vscode/main.ts
@@ -4,8 +4,4 @@ const vscode = new VSCode({
     rootDir: "..",
 });
 
-// vscode.fetch = lastlogin(vscode.fetch, {
-//     publicRoutes: ["/api/*", "/manifest.json", "/openapi.json", "/openapi.ts"],
-// });
-
 export default vscode;
