b5dcc5d46e5843958ee3732cd24543ea4be059e1
pomdtr <achille.lacoin@gmail.com>
2024-11-19 00:48:43 +0100

    19 Nov 2024, 00:48

--- vscode-dev/pkg/static/mod.ts
+++
@@ -1,8 +0,0 @@
-import { Embeds } from "jsr:@smallweb/embed@0.0.15/embed";
-
-const embeds = new Embeds({
-  "index.html": () => import("./_index.html.ts"),
-  "manifest.json": () => import("./_manifest.json.ts"),
-});
-
-export default embeds;
