162c18cf5581c2c2c16bfbe2d10e7063134c3f36
pomdtr <achille.lacoin@gmail.com>
2024-12-10 12:30:38 +0100

    10 Dec 2024, 12:30

--- vscode/main.ts
+++ vscode/main.ts
@@ -1,10 +1,7 @@
-import { VSCode } from "jsr:@smallweb/vscode@0.1.0"
+import { VSCode } from "jsr:@smallweb/vscode@0.1.6"
 
 const vscode = new VSCode({
-    rootDir: Deno.env.get("SMALLWEB_DIR"),
-    lastlogin: {
-        publicRoutes: ["/manifest.json"]
-    }
+    rootDir: Deno.env.get("SMALLWEB_DIR")
 });
 
 export default vscode;
