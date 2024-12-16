626367e0f80b3f30e1158079808dcacb823615e6
pomdtr <achille.lacoin@gmail.com>
2024-12-16 14:48:55 +0100

    16 Dec 2024, 14:48

--- install/main.ts
+++ install/main.ts
@@ -1,15 +1,16 @@
 import { serveFile } from "jsr:@std/http@1.0.12/file-server"
 import vento from "jsr:@vento/vento@1.12.10";
 
+const { GITHUB_TOKEN } = Deno.env.toObject();
+
 function fetchLatestRelease() {
     const url = new URL("https://api.github.com/repos/pomdtr/smallweb/releases/latest");
     return fetch(url, {
         headers: {
-            Authorization: `Bearer ${Deno.env.get("GITHUB_TOKEN")}`,
+            Authorization: `Bearer ${GITHUB_TOKEN}`,
         },
     });
 }
-
 
 export default {
     async fetch(req: Request) {
@@ -42,7 +43,7 @@ export default {
         const env = vento();
         const script = await env.run("install.sh", { version, target_dir: url.searchParams.get("target_dir") });
         return new Response(script.content, {
-            headers: { "Content-Type": "text/plain" },
+            headers: { "Content-Type": "text/plain; charset=utf-8" },
         });
     },
 };
