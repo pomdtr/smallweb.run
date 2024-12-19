e37a13f867803ee3486b8c6d45be1d7fcddbc8f3
pomdtr <achille.lacoin@gmail.com>
2024-12-20 00:08:40 +0100

    20 Dec 2024, 00:08

--- esm/main.ts
+++ esm/main.ts
@@ -7,6 +7,10 @@ export default {
             return Response.redirect(new URL("/esm/main.ts", req.url))
         }
 
+        if (url.searchParams.has("v")) {
+            return fetch(`https://raw.githubusercontent.com/${repo}/${url.searchParams.get("v")}${url.pathname}`)
+        }
+
         return fetch(`https://raw.githubusercontent.com/${repo}/refs/heads/main${url.pathname}`)
     }
 }
--- esm/test.ts
+++
@@ -1,1 +0,0 @@
-export { default } from "https://esm.smallweb.run/discord/main.ts"
