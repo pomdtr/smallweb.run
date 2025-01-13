9973aae36ee80f98004304ac5b7aea35250514fb
pomdtr <achille.lacoin@gmail.com>
2025-01-13 18:15:07 +0100

    13 Jan 2025, 18:15

--- auth/main.ts
+++ auth/main.ts
@@ -10,6 +10,7 @@ import { THEME_SST } from "@openauthjs/openauth/ui/theme"
 import { createSubjects } from "@openauthjs/openauth/subject"
 import * as fs from "@std/fs"
 
+await fs.ensureDir("./data")
 
 const { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET, RESEND_API_KEY } = Deno.env.toObject()
 if (!GITHUB_CLIENT_ID || !GITHUB_CLIENT_SECRET || !RESEND_API_KEY) {
@@ -64,7 +65,4 @@ const auth = issuer({
     }
 })
 
-if (await fs.exists("./data")) {
-    await Deno.mkdir("./data")
-}
 export default auth
